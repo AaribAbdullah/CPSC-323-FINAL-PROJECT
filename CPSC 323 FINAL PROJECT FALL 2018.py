81adfa1e168426a31e8b7aa87269fe75937bb82a
Aarib Abdullah <aarib@csu.fullerton.edu>
2018-12-04 00:12:48 -0800

    Just killed the comments, should look cleaner
    If (you have any quetions):
        Then Tell me!

--- CPSC 323 FINAL PROJECT FALL 2018.py
+++ CPSC 323 FINAL PROJECT FALL 2018.py
@@ -37,16 +37,12 @@ output_to_file2 = open('finalp2.txt', 'w')
 read_line = ''
 
 while read_line != 'end':
-    # while (read_line[0:2] != '/*' and read_line[len(read_line)-2:] != '*/') or read_line[0:2] != '//':
 
     read_line = file1.readline()            #This will read a line from the .txt file
     read_line = read_line.strip()              # This will strip whitespaces, \t s, and \n s from both sides of the string in variable
     read_line = read_line.replace('\t', '')  #It will remove any \t characters that still exist in the string
 
     read_line = RemoveWhiteSpaces(read_line) # will remove any excess whitespaces
-
-    index_where_semicolon_is = 0
-    is_there_a_semicolon = False
 
     are_there_comments = False
     comments_start_index = 0
@@ -58,23 +54,10 @@ while read_line != 'end':
 
         for i in range(len(read_line)):
 
-            # if read_line[i] == ';':
-            #     index_where_semicolon_is = i
-            #     is_there_a_semicolon = True
-
             if read_line[i - 2:i] == '//':
                 are_there_comments = True
                 comments_start_index = i - 2
 
-        # if is_there_a_semicolon:
-        #         #     read_line = read_line[0:index_where_semicolon_is + 1]
-        #         #     output_to_file2.write(read_line + '\n')
-        #         # else:
-        #         #     if are_there_comments:
-        #         #         read_line = read_line[0:comments_start_index]
-        #         #         output_to_file2.write(read_line + '\n')
-        #         #     else:
-        #         #         output_to_file2.write(read_line + '\n')
         if are_there_comments:
             read_line = read_line[0:comments_start_index]
             output_to_file2.write(read_line + '\n')
